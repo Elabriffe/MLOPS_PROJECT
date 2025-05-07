2c603e09f994a31362d48e646195c1e1ec14893c
Elabriffe <e.labriffe@gmail.com>
2025-05-07 18:42:19 +0200

    test pipeline

---
+++ test_api.py
@@ -0,0 +1,13 @@
+import requests
+
+def test_api_status_code():
+    response = requests.get("http://localhost:8000/docs")
+    assert response.status_code == 200
+	
+def test_api_status_code_train():
+    response = requests.get("http://localhost:8000/training/")
+    assert response.status_code == 200
+	
+def test_api_status_code_predict():
+    response = requests.get("http://localhost:8000/prediction/")
+    assert response.status_code == 200
